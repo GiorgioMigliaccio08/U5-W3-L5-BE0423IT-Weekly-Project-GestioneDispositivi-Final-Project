84f17118a2be21ccd3b9c858fe4f20f8a2ac35f0
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-26 10:44:04 +0100

    Initial commit

---
+++ src/test/java/giorgiomigliaccio/U5W3L5/U5W3L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package giorgiomigliaccio.U5W3L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
